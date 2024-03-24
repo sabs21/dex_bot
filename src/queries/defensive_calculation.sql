dc829d0f695ab98dbd2b3d7c0edb71efad5e9eb7
sabs21 <nms00010101@outlook.com>
2024-03-23 20:47:37 -0400

    fixed defensive calculations for single typed pokemon

--- src/queries/defensive_calculation.sql
+++ src/queries/defensive_calculation.sql
@@ -2,7 +2,7 @@ select
     t.name as [attacking_type],
     (select name from types tx where bt.type = tx.id) as [type1],
     (select name from types ty where bt2.type = ty.id) as [type2],
-    (bt.effectiveness * bt2.effectiveness) as [effectiveness]
+    (case when (bt.type = bt2.type) then bt.effectiveness else (bt.effectiveness * bt2.effectiveness) end) as [effectiveness]
 from
     base_types bt,
     base_types bt2
